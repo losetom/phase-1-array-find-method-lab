2dc2f78c2c6dc940215ad248f123c9b290eb4651
Mike Loseto <losetom1998@gmail.com>
2022-07-21 17:54:12 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+]
+
+function superbowlWin(record) {
+    for(let item of record) {
+        if(item.result === 'W'){
+            return item.year
+        }
+    }
+}
